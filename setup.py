008f1142f45c568b9930e3e81aff6bd92014f993
Xavier SCHMITT <xavier.schmitt@nist.gov>
2019-03-18 11:42:26 -0400

    chore: upgrade 1.1.0
    Change-Id: I44eb8e500309846473249ad12915b1688df434ef

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_module_periodic_table_app",
-    version="1.0.1",
+    version="1.1.0",
     description="Periodic table module for the parser core project",
     long_description=long_desc,
     author="NIST IT Lab",
